2cd546964416ebb21102e566b902d960ba724f73
nicole <nicolekaneshige@gmail.com>
2024-05-19 08:54:19 -0700

    add move constructor in chessEngine.py

--- chessEngine.py
+++ chessEngine.py
@@ -23,4 +23,26 @@ class GameState():
         self.moveLog = []
 
 class Move():
+    # maps keys to values
+    # key : value
+    ranksToRows = {"1" : 7, "2" : 6, "3" : 5, "4" : 4,
+                   "5" : 3, "6" : 2, "7" : 1, "8" : 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()} #switch mapping in ranksToRows => for each of the k , v make a v , k pair 
+    filesToCols = {"a" : 0, "b" : 1, "c" : 2, "d", 3,
+                   "e" : 4, "f" : 5, "g" : 6, "h" : 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
     
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+        
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
